1b2bf2a5d39b5a330d015838a9dcf2e96dd84c22
naveed-islam40 <naveedislam20ni@gmail.com>
2024-11-07 19:13:20 +0500

    change

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,8 +8,8 @@ router.post('/signup', userController.SignUp);
 router.post('/login', userController.Login);
 router.put('/update', isAuthenticated.isAuthenticated, userController.UpdateUser);
 router.get('/details', isAuthenticated.isAuthenticated, userController.getUserDetails);
-router.post('/reset/email', isAuthenticated.isAuthenticated, userController.ForgotPasswordEmail);
-router.post('/reset/:token', isAuthenticated.isAuthenticated, userController.ResetPassword);
+router.post('/reset/email', userController.ForgotPasswordEmail);
+router.post('/reset/:token', userController.ResetPassword);
 router.delete('/:userId', isAuthenticated.isAuthenticated, userController.DeleteUser);
 
 
