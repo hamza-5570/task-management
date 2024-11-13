b7dd9b065c885e9456a8d036e712d177daf69f1c
naveed-islam40 <naveedislam20ni@gmail.com>
2024-11-13 12:00:22 +0500

    change

--- services/projectService.js
+++ services/projectService.js
@@ -14,6 +14,7 @@ class ProjectService{
         const skip = (page - 1) * limit;
 
         const projects = await Project.find({ created_by:userId })
+            .populate("tasks")
             .skip(skip)
             .limit(limit)
             .exec();
