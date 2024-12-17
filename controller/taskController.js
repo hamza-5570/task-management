ccf33c6eda5a6faa4d5553d15677f0482731afc1
naveed-islam40 <naveedislam20ni@gmail.com>
2024-12-17 12:20:29 +0500

    updates

--- controller/taskController.js
+++ controller/taskController.js
@@ -62,13 +62,45 @@ class TaskController {
         return Response.serverError(res, messageUtil.FAILED_TO_FETCH_TASKS);
       }
 
+      // [formate the date]
+      const formatDateToString = (date, time) => {
+        const [hours, minutes] = time.split(":");
+        const formattedDate = new Date(date);
+
+        formattedDate.setHours(hours, minutes, 0, 0);
+
+        const formattedTime =
+          formattedDate.getFullYear() +
+          "-" +
+          String(formattedDate.getMonth() + 1).padStart(2, "0") +
+          "-" +
+          String(formattedDate.getDate()).padStart(2, "0") +
+          "T" +
+          String(formattedDate.getHours()).padStart(2, "0") +
+          ":" +
+          String(formattedDate.getMinutes()).padStart(2, "0");
+
+        return formattedTime;
+      };
+
+      const getEndTime = (date) => {
+        const extractedDate = new Date(date).toISOString().substring(0, 10);
+        const endofDay = new Date(extractedDate);
+        endofDay.setUTCHours(23, 59, 59, 999);
+
+        return endofDay.toISOString().substring(0, 16);
+      }
+
       // [total worked hours]
       const tasksWithWorkedHours = await Promise.all(
         tasks.map(async (task) => {
           const { totalHours } = await WorkedHoursServices.getWorkedHoursForTask({ task: task._id });
+
           return {
             ...task.toObject(),
             totalWorkedHours: totalHours || 0,
+            from: formatDateToString(task.due_date, task.time),
+            to: getEndTime(task.due_date),
           };
         })
       );
