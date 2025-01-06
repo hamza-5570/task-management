d19a14ba92212186bc6813e229629f7390ba6a13
hamza-5570 <hafizameerhamza87@gmail.com>
2025-01-06 19:02:39 +0500

    changes

--- services/InvoiceService.js
+++ services/InvoiceService.js
@@ -1,42 +1,51 @@
 import Invoice from "../model/invoices.js";
 
-
 class InvoiceService {
-    createInvoice = async (invoice) => {
-        return await Invoice.create(invoice);
-    }
-    getInvoices = async (userId, page, limit, status) => {
-        const skip = (page - 1) * limit;
-        const invoices = await Invoice.find({ created_by: userId, status }).skip(skip).limit(limit).exec();
-        const totalCount = await Invoice.countDocuments({ created_by: userId, status });
+  createInvoice = async (invoice) => {
+    return await Invoice.create(invoice);
+  };
+  getInvoices = async (userId, page, limit, status) => {
+    const skip = (page - 1) * limit;
+    const invoices = await Invoice.find({ created_by: userId, status })
+      .populate({ path: "project", select: "_id project_name" })
+      .skip(skip)
+      .limit(limit)
+      .exec();
+    const totalCount = await Invoice.countDocuments({
+      created_by: userId,
+      status,
+    });
 
-        return { invoices, totalCount };
-    }
+    return { invoices, totalCount };
+  };
 
-    getInvoicesByUserId = async (userId, page, limit) => {
-        console.log(userId, page, limit);
+  getInvoicesByUserId = async (userId, page, limit) => {
+    console.log(userId, page, limit);
 
-        const skip = (page - 1) * limit;
-        const invoices = await Invoice.find({ created_by: userId }).skip(skip).limit(limit).exec();
-        const totalCount = await Invoice.countDocuments({ created_by: userId });
-        return { invoices, totalCount };
-    }
-    getPaidInvoices = async (userId) => {
-        return await Invoice.find({ status: "Paid", created_by: userId });
-    }
+    const skip = (page - 1) * limit;
+    const invoices = await Invoice.find({ created_by: userId })
+      .skip(skip)
+      .limit(limit)
+      .exec();
+    const totalCount = await Invoice.countDocuments({ created_by: userId });
+    return { invoices, totalCount };
+  };
+  getPaidInvoices = async (userId) => {
+    return await Invoice.find({ status: "Paid", created_by: userId });
+  };
 
-    getUnPaidInvoices = async (userId) => {
-        return await Invoice.find({ status: "Unpaid", created_by: userId });
-    }
-    getInvoice = async (query) => {
-        return await Invoice.findOne(query);
-    }
-    updateInvoice = async (data, query) => {
-        return await Invoice.findOneAndUpdate(query, data, { new: true });
-    }
-    deleteInvoice = async (query) => {
-        return await Invoice.findOneAndDelete(query);
-    }
+  getUnPaidInvoices = async (userId) => {
+    return await Invoice.find({ status: "Unpaid", created_by: userId });
+  };
+  getInvoice = async (query) => {
+    return await Invoice.findOne(query);
+  };
+  updateInvoice = async (data, query) => {
+    return await Invoice.findOneAndUpdate(query, data, { new: true });
+  };
+  deleteInvoice = async (query) => {
+    return await Invoice.findOneAndDelete(query);
+  };
 }
 
-export default new InvoiceService()
+export default new InvoiceService();
